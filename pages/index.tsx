d9b800ca636788c44f7ef04ed3747fe58ff74b3a
Yoba Fett <alexka.680@gmail.com>
2022-12-14 20:04:08 +0900

    Add Htag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import React from 'react';
+import { Htag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-sdaasdasd
-    </div>
+    <>
+      <Htag tag='h1'>qwe</Htag>
+      <Htag tag='h2'>qwe</Htag>
+      <Htag tag='h3'>qwe</Htag>
+    </>
   );
 }
