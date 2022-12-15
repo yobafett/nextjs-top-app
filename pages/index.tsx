04a56669aa3d0668de7e519311b9e5245ccf750c
Yoba Fett <alexka.680@gmail.com>
2022-12-15 19:14:04 +0900

    Add button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Htag } from '../components';
+import { Htag, Button } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -7,6 +7,8 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>qwe</Htag>
       <Htag tag='h2'>qwe</Htag>
       <Htag tag='h3'>qwe</Htag>
+      <Button appearance='primary'>qwe</Button>
+      <Button appearance='ghost'>qwe</Button>
     </>
   );
 }
