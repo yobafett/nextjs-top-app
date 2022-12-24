76071924644f6ce186674fa04f9d74255146bf0f
Yoba Fett <alexka.680@gmail.com>
2022-12-24 11:20:29 +0900

    Improve Layout HOC

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { Htag, Button, Ptag, Tag, Rating } from '../components';
-import { Layout } from '../layout/Layout';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
-    <Layout>
+    <>
       <Htag tag='h1'>qwe</Htag>
       <Htag tag='h2'>qwe</Htag>
       <Htag tag='h3'>qwe</Htag>
@@ -24,6 +24,8 @@ export default function Home(): JSX.Element {
       <Tag size='m' color='red'>Five</Tag>
       <Rating rating={rating} isEditable={true} setRating={setRating}/>
       <Rating rating={3} />
-    </Layout>
+    </>
   );
 }
+
+export default withLayout(Home);
