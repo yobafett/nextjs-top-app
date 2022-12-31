27a05ef9b9163faf2d961d5e25388a3abcdf0cb3
Yoba Fett <alexka.680@gmail.com>
2022-12-31 14:33:45 +0900

    Add Footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
+import classNames from 'classnames';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer {...props} className={classNames(className, styles.footer)} >
+            <div>OwlTop © 2020 - {currentYear} Все права защищены</div>
+            <a href='/' target='_blank'>Пользовательское соглашение</a>
+            <a href='/' target='_blank'>Политика конфиденциальности</a>
+        </footer>
     )
 }
