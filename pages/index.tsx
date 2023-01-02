0c383ed6bde800f73079cdfc22a3b61303dfb041
Yoba Fett <alexka.680@gmail.com>
2023-01-02 12:55:31 +0900

    Get menu items from API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from 'next';
 import React, { useState } from 'react';
 import { Htag, Button, Ptag, Tag, Rating } from '../components';
 import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -22,10 +25,32 @@ function Home(): JSX.Element {
       <Tag size='m' color='green'>Three</Tag>
       <Tag size='m' color='gray'>Four</Tag>
       <Tag size='m' color='red'>Five</Tag>
-      <Rating rating={rating} isEditable={true} setRating={setRating}/>
+      <Rating rating={rating} isEditable={true} setRating={setRating} />
       <Rating rating={3} />
+      <ul>
+        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory: firstCategory
+  })
+
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number
+}
